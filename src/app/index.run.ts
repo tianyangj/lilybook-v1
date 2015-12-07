b60cabb215e1d2060c093e8958fcd7b066fc3919
Tommy Jiang <tianyangj@gmail.com>
2015-12-06 20:15:16 -0800

    add parse

--- src/app/index.run.ts
+++ src/app/index.run.ts
@@ -1,4 +1,6 @@
 /** @ngInject */
 export function runBlock($log: angular.ILogService) {
   $log.debug('runBlock end');
+
+  Parse.initialize('fHO4LtJRfsdhQBBicYZpdpj3BQHHQCVEiDPkS4ZI', '3gzRyAZnxtQLn1IofC4Layn6cc487e4n5Jin6FzM');
 }
