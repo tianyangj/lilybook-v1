8d80a174693768aafee2f7eefd0768a80baeed43
Tommy Jiang <tianyangj@gmail.com>
2015-12-08 21:36:09 -0800

    login form

--- src/app/index.route.ts
+++ src/app/index.route.ts
@@ -10,6 +10,10 @@ export function routerConfig($stateProvider: angular.ui.IStateProvider, $urlRout
       url: '/',
       templateUrl: 'app/splash/splash.html'
     })
+    .state('app.login', {
+      url: '/login',
+      templateUrl: 'app/login/login.html'
+    })
     .state('app.home', {
       url: '/home',
       templateUrl: 'app/main/main.html',
