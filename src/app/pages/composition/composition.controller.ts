8886221e2620e2bc0c370cf8e28e47a0fd8e2a6c
Tommy Jiang <tianyangj@gmail.com>
2015-12-27 21:49:47 -0800

    composition page

--- src/app/pages/composition/composition.controller.ts
+++ src/app/pages/composition/composition.controller.ts
@@ -1,4 +1,18 @@
+import { IComposition } from '../../services/composition.model';
+import { CompositionService } from '../../services/composition.service';
 
 export class CompositionController {
 
+    composition: IComposition;
+
+    /** @ngInject */
+    constructor(
+        private $stateParams: any,
+        private compositionService: CompositionService
+    ) {
+        this.compositionService.getComposition($stateParams.id).then((composition: IComposition) => {
+            this.composition = composition;
+            console.log(composition);
+        });
+    }
 }
