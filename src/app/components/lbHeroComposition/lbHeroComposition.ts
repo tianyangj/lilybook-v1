2b0bf8f6abe4ade162a01175e1982188e86ada33
Tommy Jiang <tjiang@weilis-MacBook-Pro.local>
2016-01-02 21:52:30 -0800

    fix image url

--- src/app/components/lbHeroComposition/lbHeroComposition.ts
+++ src/app/components/lbHeroComposition/lbHeroComposition.ts
@@ -5,7 +5,7 @@ export function lbHeroComposition(): angular.IDirective {
 			<div ng-if="video" style="height:360px;min-height:360px;">
 				<youtube-video video-url="video" player-width="'100%'" player-height="'360px'" player-vars="{autoplay:1,showinfo:0}"></youtube-video>
 			</div>
-			<div ng-if="!video" style="height:240px;min-height:240px;background-image:url(/images/sheet-music.jpg);background-size:cover;background-repeat:no-repeat;background-position:center center;"></div>
+			<div ng-if="!video" style="height:240px;min-height:240px;background-image:url(/assets/images/sheet-music.jpg);background-size:cover;background-repeat:no-repeat;background-position:center center;"></div>
 		`,
         scope: {
             video: '='
