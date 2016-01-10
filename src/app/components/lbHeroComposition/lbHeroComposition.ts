59d837fa0696b069dfa4abf7db8f40ecf7313141
Tommy Jiang <tjiang@weilis-MacBook-Pro.local>
2016-01-10 13:25:53 -0800

    add hero close button

--- src/app/components/lbHeroComposition/lbHeroComposition.ts
+++ src/app/components/lbHeroComposition/lbHeroComposition.ts
@@ -1,14 +1,15 @@
 export function lbHeroComposition(): angular.IDirective {
     return {
         restrict: 'E',
-        template: `
-			<div ng-if="video" style="height:360px;min-height:360px;">
-				<youtube-video video-url="video" player-width="'100%'" player-height="'360px'" player-vars="{autoplay:1,showinfo:0}"></youtube-video>
-			</div>
-			<div ng-if="!video" style="height:240px;min-height:240px;background-image:url(/assets/images/sheet-music.jpg);background-size:cover;background-repeat:no-repeat;background-position:center center;"></div>
-		`,
+        templateUrl: 'app/components/lbHeroComposition/hero-composition.html',
         scope: {
             video: '='
+        },
+        replace: true,
+        link: (scope: any, element: angular.IAugmentedJQuery) => {
+            scope.close = () => {
+                element.remove();
+            };
         }
     };
 }
