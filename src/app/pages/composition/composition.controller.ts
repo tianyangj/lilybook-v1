bf5d69bf36e2cfd4936e8862c434e9995ebc7d40
Tommy Jiang <tianyangj@gmail.com>
2016-01-11 20:23:59 -0800

    add composition stats

--- src/app/pages/composition/composition.controller.ts
+++ src/app/pages/composition/composition.controller.ts
@@ -1,11 +1,35 @@
+import { IActivity, ActivityType } from '../../services/activity.model';
 import { IComposition } from '../../services/composition.model';
+import { ActivityService } from '../../services/activity.service';
 
 export class CompositionController {
 
+    likes: IActivity[];
+    todos: IActivity[];
+    average: number;
+
     /** @ngInject */
     constructor(
-        private composition: IComposition
+        private composition: IComposition,
+        private activityService: ActivityService
     ) {
-        console.log(composition);
+        activityService.list(composition).then((activities: IActivity[]) => {
+            this.likes = activities.filter((activity: IActivity) => {
+                return activity.type === ActivityType.Like;
+            });
+            this.todos = activities.filter((activity: IActivity) => {
+                return activity.type === ActivityType.Todo;
+            });
+            var difficulties = activities.filter((activity: IActivity) => {
+                return activity.type === ActivityType.Difficulty;
+            }).map((activity: IActivity) => {
+                return activity.meta.difficulty;
+            });
+            if (difficulties.length) {
+                this.average = difficulties.reduce((a: number, b: number) => {
+                    return a + b;
+                }, 0) / difficulties.length;
+            }
+        });
     }
 }
