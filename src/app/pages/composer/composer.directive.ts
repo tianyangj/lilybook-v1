b2608814a7f9beb645d5c6f3d0e6ba1fafb78804
Tommy Jiang <tjiang@weilis-MacBook-Pro.local>
2016-01-22 01:08:56 -0800

    composer page style update

--- src/app/pages/composer/composer.directive.ts
+++ src/app/pages/composer/composer.directive.ts
@@ -1,13 +1,12 @@
 import { IComposition } from '../../services/composition.model';
 import { IComposer } from '../../services/composer.model';
-import { IForm } from '../../services/form.model';
-import { ICompositionQuery, CompositionService } from '../../services/composition.service';
+import { CompositionService } from '../../services/composition.service';
 
 export default function lbComposerPage(): angular.IDirective {
 
     return {
         restrict: 'E',
-        templateUrl: 'app/pages/composer/composer1.html',
+        templateUrl: 'app/pages/composer/composer.html',
         scope: {},
         controller: ComposerPageController,
         controllerAs: 'composerCtrl',
@@ -19,10 +18,7 @@ export default function lbComposerPage(): angular.IDirective {
 class ComposerPageController {
 
     composer: IComposer;
-    compositions: IComposition[];
     compositionsByForm: { [index: string]: IComposition[] };
-    query: ICompositionQuery = {};
-    loading = false;
 
     /** @ngInject */
     constructor(
@@ -32,35 +28,12 @@ class ComposerPageController {
     ) {
         this.compositionService.getComposer($stateParams.vanity).then((composer: IComposer) => {
             this.composer = composer;
-            _.extend(this.query, { composerId: composer.id });
-            this.loading = true;
-            this.compositionService.getCompositions(this.query).then((compositions: IComposition[]) => {
-                console.log(compositions);
-                this.compositions = compositions;
+            this.compositionService.getCompositions({ composerId: composer.id }).then((compositions: IComposition[]) => {
                 this.compositionsByForm = _.groupBy(compositions, (composition: IComposition) => {
                     return composition.form.name;
                 });
-                console.log(this.compositionsByForm)
-                this.loading = false;
             });
-        });
-        $rootScope.$on('selectFormChanged', (event: angular.IAngularEvent, form: IForm) => {
-            _.extend(this.query, { formId: form.id });
-            this.loading = true;
-            this.updateCompositions();
-        });
-        $rootScope.$on('selectSortChanged', (event: angular.IAngularEvent, sort: any) => {
-            _.extend(this.query, { sortId: sort.value });
-            this.loading = true;
-            this.updateCompositions();
         });
     }
 
-    private updateCompositions = _.debounce(() => {
-        // console.log('query', this.query);
-        this.compositionService.getCompositions(this.query).then((compositions: IComposition[]) => {
-            this.compositions = compositions;
-            this.loading = false;
-        });
-    }, 600);
 }
